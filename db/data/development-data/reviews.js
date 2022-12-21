e1b3c117a54901e0dfc71fe38e4f6de4557163d7
Dave Ridealgh <davecodingstuff@gmail.com>
2022-12-21 10:19:22 +0000

    standardise dev data image dimensions

--- db/data/development-data/reviews.js
+++ db/data/development-data/reviews.js
@@ -4,9 +4,9 @@ module.exports = [
     designer: 'Uwe Rosenberg',
     owner: 'tickle122',
     review_img_url:
-      'https://images.pexels.com/photos/4917821/pexels-photo-4917821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/974314/pexels-photo-974314.jpeg?w=700&h=700',
     review_body:
-      "You could sum up Agricola with the simple phrase 'Farmyeard Fun' but the mechanics and game play add so much more than that. You'll find yourself torn between breeding pigs, or sowing crops. Its joyeous and rewarding and it makes you think of time spent outside, which is much harder to do these days!",
+      "You could sum up Agricola with the simple phrase 'Farmyard Fun' but the mechanics and game play add so much more than that. You'll find yourself torn between breeding pigs, or sowing crops. Its joyeous and rewarding and it makes you think of time spent outside, which is much harder to do these days!",
     category: 'strategy',
     created_at: new Date(1610964020514),
     votes: 1
@@ -16,7 +16,7 @@ module.exports = [
     designer: 'Leslie Scott',
     owner: 'grumpy19',
     review_img_url:
-      'https://images.pexels.com/photos/4009761/pexels-photo-4009761.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      'https://images.pexels.com/photos/4473494/pexels-photo-4473494.jpeg?w=700&h=700',
     review_body:
       "Few games are equiped to fill a player with such a defined sense of mild-peril, but a friendly game of Jenga will turn the mustn't-make-it-fall anxiety all the way up to 11! Fiddly fun for all the family, this game needs little explaination. Whether you're a player who chooses to play it safe, or one who lives life on the edge, eventually the removal of blocks will destabilise the tower and all your Jenga dreams come tumbling down.",
     category: 'dexterity',
@@ -28,7 +28,7 @@ module.exports = [
     designer: 'Rikki Tahta',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/45868/chameleon-reptile-lizard-green-45868.jpeg?w=700&h=700',
     review_body:
       'Try to trick your friends. If you find yourself being dealt the Chamelean card then the aim of the game is simple; blend in... Meanwhile the other players aim to be as vague as they can to not give the game away ',
     category: 'hidden-roles',
@@ -40,7 +40,7 @@ module.exports = [
     designer: 'Akihisa Okui',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?w=700&h=700',
     review_body: "We couldn't find the werewolf!",
     category: 'hidden-roles',
     created_at: new Date(1610964101251),
@@ -51,7 +51,7 @@ module.exports = [
     designer: 'Wolfgang Warsch',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/279321/pexels-photo-279321.jpeg?w=700&h=700',
     review_body:
       "Ever wish you could try your hand at mixing potions? Quacks of Quedlinburg will have you mixing up a homebrew like no other. Each player buys different ingredients (chips) that are drawn at random to reach the most points, but watch out, you'd better not let your cauldrom explode.",
     category: 'push-your-luck',
@@ -63,7 +63,7 @@ module.exports = [
     designer: 'Asger Harding Granerud',
     owner: 'cooljmessy',
     review_img_url:
-      'https://images.pexels.com/photos/258045/pexels-photo-258045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/258045/pexels-photo-258045.jpeg?w=700&h=700',
     review_body:
       'Cold rain pours on the faces of your team of cyclists, you pulled to the front of the pack early and now your taking on exhaustion cards like there is not tomorrow, you think there are about 2 hands left until you cross the finish line, will you draw enough from your deck to cross before the other team shoot passed? Flamee Rouge is a Racing deck management game where you carefully manage your deck in order to cross the line before your opponents, cyclist can fall slyly behind front runners in their slipstreams to save precious energy for the prefect moment to burst into the lead ',
     category: 'deck-building',
@@ -75,7 +75,7 @@ module.exports = [
     designer: 'Fiona Lohoar',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/220057/pexels-photo-220057.jpeg?w=700&h=700',
     review_body:
       "If you've ever wanted to accuse your siblings, cousins or friends of being part of a plot to murder everyone whilst secretly choosing which one of them should get the chop next - this is the boardgame for you. Buyer beware: once you gain a reputation for being able to lie with a stone face about being the secret killer you may never lose it.",
     category: 'hidden-roles',
@@ -87,7 +87,7 @@ module.exports = [
     designer: 'Jamey Stegmaier',
     owner: 'grumpy19',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/4200740/pexels-photo-4200740.jpeg?w=700&h=700',
     review_body:
       'Spend 30 minutes just setting up all of the boards (!) meeple and decks, just to forget how to play. Scythe can be a lengthy game but really packs a punch if you put the time in. With beautiful artwork, countless scenarios and clever game mechanics, this board game is a must for any board game fanatic; just make sure you explain ALL the rules before you start playing with first timers or you may find they bring it up again and again.',
     category: 'engine-building',
@@ -111,7 +111,7 @@ module.exports = [
     designer: 'Gamey McGameface',
     owner: 'jessjelly',
     review_img_url:
-      'https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/4691579/pexels-photo-4691579.jpeg?w=700&h=700',
     review_body:
       'Consequat velit occaecat voluptate do. Dolor pariatur fugiat sint et proident ex do consequat est. Nisi minim laboris mollit cupidatat et adipisicing laborum do. Sint sit tempor officia pariatur duis ullamco labore ipsum nisi voluptate nulla eu veniam. Et do ad id dolore id cillum non non culpa. Cillum mollit dolor dolore excepteur aliquip. Cillum aliquip quis aute enim anim ex laborum officia. Aliqua magna elit reprehenderit Lorem elit non laboris irure qui aliquip ad proident. Qui enim mollit Lorem labore eiusmod',
     category: 'dexterity',
@@ -123,7 +123,7 @@ module.exports = [
     designer: 'Seymour Buttz',
     owner: 'grumpy19',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/6333891/pexels-photo-6333891.jpeg?w=700&h=700',
     review_body:
       'Labore occaecat sunt qui commodo anim anim aliqua adipisicing aliquip fugiat. Ad in ipsum incididunt esse amet deserunt aliqua exercitation occaecat nostrud irure labore ipsum. Culpa tempor non voluptate reprehenderit deserunt pariatur cupidatat aliqua adipisicing. Nostrud labore dolor fugiat sint consequat excepteur dolore irure eu. Anim ex adipisicing magna deserunt enim fugiat do nulla officia sint. Ex tempor ut aliquip exercitation eiusmod. Excepteur deserunt officia voluptate sunt aliqua esse deserunt velit. In id non proident veniam ipsum id in consequat duis ipsum et incididunt. Qui cupidatat ea deserunt magna proident nisi nulla eiusmod aliquip magna deserunt fugiat fugiat incididunt. Laboris nisi velit mollit ullamco deserunt eiusmod deserunt ea dolore veniam.',
     category: 'engine-building',
@@ -135,7 +135,7 @@ module.exports = [
     designer: 'Ollie Tabooger',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/6333934/pexels-photo-6333934.jpeg?w=700&h=700',
     review_body:
       'Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.',
     category: 'roll-and-write',
@@ -147,7 +147,7 @@ module.exports = [
     designer: 'Avery Wunzboogerz',
     owner: 'tickle122',
     review_img_url:
-      'https://images.pexels.com/photos/278888/pexels-photo-278888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/411207/pexels-photo-411207.jpeg?w=700&h=700',
     review_body:
       "Don't underestimate the tension and supsense that can be brought on with a round of Kerplunk! You'll feel the rush and thrill of not disturbing the stack of marbles, and probably utter curse words when you draw the wrong straw. Fanily friendly, and not just for kids! ",
     category: 'dexterity',
@@ -159,7 +159,7 @@ module.exports = [
     designer: 'Don Keigh',
     owner: 'cooljmessy',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/8111357/pexels-photo-8111357.jpeg?w=700&h=700',
     review_body:
       'Nostrud anim cupidatat incididunt officia cupidatat magna. Cillum commodo voluptate laboris id incididunt esse elit ipsum consectetur non elit elit magna. Aliquip sint amet eiusmod magna. Fugiat non ut ex eiusmod elit. Esse anim irure laborum aute ut ad reprehenderit. Veniam laboris dolore mollit mollit in. Cillum in aliquip adipisicing ipsum et dolor veniam qui ut ullamco aliquip in. Dolor fugiat elit laborum elit cupidatat aute qui nostrud. Duis incididunt ea nostrud minim consequat. Reprehenderit mollit cupidatat do culpa aliqua culpa mollit minim eiusmod. Deserunt occaecat ipsum ex ut pariatur eu veniam cillum nulla ex nostrud. Do nostrud amet duis proident nostrud eiusmod occaecat reprehenderit. Quis et cupidatat tempor qui dolor id veniam in sunt ipsum eiusmod. Sint tempor commodo consectetur mollit proident culpa nulla est tempor ullamco tempor aliquip laboris.',
     category: 'hidden-roles',
@@ -171,7 +171,7 @@ module.exports = [
     designer: 'Word Smith',
     owner: 'jessjelly',
     review_img_url:
-      'https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/8205368/pexels-photo-8205368.jpeg?w=700&h=700',
     review_body:
       "You know; the one that looks a lot like Scrabble, and plays a lot like Scrabble, but you have to push the letter tiles out of cardboard becasue that makes it more \"fun\"...  If you're a fan of words you'll love this game regardless, but if you're a hard core word nerd you probably have the original anyway.",
     category: 'strategy',
@@ -183,7 +183,7 @@ module.exports = [
     designer: 'Alan R. Moon',
     owner: 'weegembump',
     review_img_url:
-      'https://images.pexels.com/photos/4691567/pexels-photo-4691567.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/4691567/pexels-photo-4691567.jpeg?w=700&h=700',
     review_body:
       'Choo-chooing onto game tables and zooming its way to becoming a modern classic. Ticket To Ride is rich with trains, tickets and tactics. Players gather train cards to enable then to build routes across a North American map, joining big cities and trying not to get blocked by their opponent ',
     category: 'deck-building',
@@ -195,7 +195,7 @@ module.exports = [
     designer: 'Uwe Rosenburg',
     owner: 'tickle122',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/279321/pexels-photo-279321.jpeg?w=700&h=700',
     review_body:
       "Loosely based around the phases of the moon, this tile placing game is rich in strategy anf makes great use of non-standard turn taking. Nova Luna was one of my favourite nominations for the prestigious Kennerspiel des Jahres in 2020. Sadly it didn't take home the title, but its an intruguing abstract game, certainly one to add to your game shelf.",
     category: 'strategy',
@@ -207,7 +207,7 @@ module.exports = [
     designer: 'Emerson Matsuuchi',
     owner: 'tickle122',
     review_img_url:
-      'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      'https://images.pexels.com/photos/6333894/pexels-photo-6333894.jpeg?w=700&h=700',
     review_body:
       'This game reminds me of the stress-free environment described in a song sung by a crab in the famous film about a mermaid. Plenty of coral collecting, and reef building to keep you entertained ',
     category: 'strategy',
@@ -219,7 +219,7 @@ module.exports = [
     designer: 'Chuck Foley',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/6630813/pexels-photo-6630813.jpeg?w=700&h=700',
     review_body:
       'Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.',
     category: 'roll-and-write',
@@ -231,7 +231,7 @@ module.exports = [
     designer: 'Uncredited',
     owner: 'grumpy19',
     review_img_url:
-      'https://images.pexels.com/photos/1314435/pexels-photo-1314435.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/4792379/pexels-photo-4792379.jpeg?w=700&h=700',
     review_body:
       'This household classic needs no introduction. Monopoly has been causeing family fallouts for close to 90 years. With numerous special editions and no doubt more still to come almost everyone has played this game, but has anyone ever finished it?',
     category: 'strategy',
@@ -243,7 +243,7 @@ module.exports = [
     designer: 'Wolfgang Warsch',
     owner: 'happyamy2016',
     review_img_url:
-      'https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/516114/pexels-photo-516114.jpeg?w=700&h=700',
     review_body:
       "The title translates to \"that's so clever\" and it won't take you long to realise why. Ganz Schon Clever is a really engagind roll-and-write where you'll try to collect score points from coloured dice. Sounds simple, but the real strategy of this game lies in how you collect the bonuses.",
     category: 'roll-and-write',
@@ -255,7 +255,7 @@ module.exports = [
     designer: 'Edwin S. Lowe',
     owner: 'grumpy19',
     review_img_url:
-      'https://images.pexels.com/photos/33968/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/3956552/pexels-photo-3956552.jpeg?w=700&h=700',
     review_body:
       'Often thought of as the ultimate roll-and-write game, You\'ll find it hard to overlook Yahtzee\'s appeal. Yahtzee is quick to teach and quick to play. Think of it as an essential "palate-cleanser" game to have in your store cupboard for those times when chunkier games leave a bitter taste in players mouths.',
     category: 'roll-and-write',
@@ -267,7 +267,7 @@ module.exports = [
     designer: 'Alex Crispin,',
     owner: 'jessjelly',
     review_img_url:
-      'https://images.pexels.com/photos/2416463/pexels-photo-2416463.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/5439508/pexels-photo-5439508.jpeg?w=700&h=700',
     review_body:
       'Escape the Dark Castle is a cooperative board game than will bring back feelings of eighties nostalgia. Released in 2017, its already well on its way to being a cult classic. Filled with dark traps, dungeons and monsterous foes the aim of the game is simple, escape!',
     category: 'push-your-luck',
@@ -279,7 +279,7 @@ module.exports = [
     designer: 'Alex Crispin,',
     owner: 'jessjelly',
     review_img_url:
-      'https://images.pexels.com/photos/596132/pexels-photo-596132.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'https://images.pexels.com/photos/3910141/pexels-photo-3910141.jpeg?w=700&h=700',
     review_body:
       "Argueably one of the best things to come out of 2020, and new addition to what will hopefully become more than a two part series. Escape the Dark Sector takes everything about Escape the Dark Castle that made it wonderful, and then adds guns! Thrust into the future by the imersive chapter cards, your mission remains the same, (thats right, escape!) but the challenges you'll face feel a whole lot more grown up. Cooperative and atmospheric, Escape the Dark Sector brings more variety and a bigger challenge to players. Don't underestimate the dabger of running out of ammo.",
     category: 'push-your-luck',
