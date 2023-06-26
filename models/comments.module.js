bdca76d76617ab89d746faa595c0243d6a375149
KarlieKKY <yanndeel@gmail.com>
2023-06-26 11:42:45 +0100

    18-patch comment by comment id

--- models/comments.module.js
+++ models/comments.module.js
@@ -1,5 +1,30 @@
 const db = require("../db/connection");
 
+exports.updateCommentById = (id, votes) => {
+  if (isNaN(id)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad request! Comment Id should be a valid number.",
+    });
+  }
+  const queryStr = `
+  UPDATE comments
+  SET 
+    votes = votes + $1
+  WHERE 
+    comment_id = $2
+  RETURNING *;  
+  `;
+
+  return db.query(queryStr, [votes, id]).then((result) => {
+    if (result.rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Comment Id not found!" });
+    } else {
+      return result.rows[0];
+    }
+  });
+};
+
 exports.removeCommentByCommentid = (id) => {
   if (isNaN(id)) {
     return Promise.reject({
--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,9 +1,13 @@
 const {
   deleteCommentByCommentid,
+  patchCommentId,
 } = require("../controllers/comments.controllers");
 
 const commentsRouter = require("express").Router();
 
-commentsRouter.route("/:comment_id").delete(deleteCommentByCommentid);
+commentsRouter
+  .route("/:comment_id")
+  .patch(patchCommentId)
+  .delete(deleteCommentByCommentid);
 
 module.exports = commentsRouter;
